94eea6d0628c2f43d1ef09775afa64fb51b39089
Charles Justine Mantes <167271675+selrahcjstn@users.noreply.github.com>
2025-02-27 15:01:48 +0800

    Section size updated

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -2,7 +2,7 @@ function SectionTitle({ title, titleDesc }) {
     return (
         <div className="flex flex-col items-center">
             <p className="text-[#aaa6c3] text-[14px] uppercase">{titleDesc}</p>
-            <h1 className="text-[52px] font-bold capitalize">{title}</h1>
+            <h1 className="text-[48px] font-bold capitalize">{title}</h1>
         </div>
     )
 }
